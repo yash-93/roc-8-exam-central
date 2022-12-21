5e5724eb6a17f528cd96265583605c2b62425909
Yashdeep Bachhas <yashdeepbachhas93@gmail.com>
2022-12-22 01:56:19 +0530

    Update slug.ts

--- backend/utils/slug.ts
+++ backend/utils/slug.ts
@@ -1,27 +1,6 @@
 import { text } from "@keystone-6/core/fields";
 import { customAlphabet } from 'nanoid';
 
-// export function slug() {
-//   const alphabet = '0123456789abcdefghijklmnopqrstuvwxyz';
-//   const nanoid = customAlphabet(alphabet, 6);
-
-//   return text({
-//     defaultValue: ({ inputData }: any) => {
-//       let input = inputData?.name || inputData?.title || 'new-item';
-//       input = input
-//         .trim()
-//         .toLowerCase()
-//         .replace(/[^\w ]+/g, '')
-//         .replace(/ +/g, '-') ?? ''
-
-//       return (
-//         input + nanoid()
-//       );
-//     },
-//     ui: { createView: { fieldMode: 'hidden' } },
-//   });
-// }
-
 export function slug() {
   const alphabet = '0123456789abcdefghijklmnopqrstuvwxyz';
   const nanoid = customAlphabet(alphabet, 6);
