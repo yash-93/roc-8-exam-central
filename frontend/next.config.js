570b7085172d44392da8a872b660069ca648d415
Yashdeep Bachhas <yashdeepbachhas93@gmail.com>
2022-12-25 02:28:07 +0530

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   images: {
     domains: ['localhost', 'images.unsplash.com'],
+  },
+  env: {
+    // SERVER_URL: 'http://localhost:3000',
+    // GRAPHQL_URL: 'http://localhost:3000/api/graphql'
+    SERVER_URL: 'https://exam-central-server.onrender.com',
+    GRAPHQL_URL: 'https://exam-central-server.onrender.com/api/graphql'
   }
 }
